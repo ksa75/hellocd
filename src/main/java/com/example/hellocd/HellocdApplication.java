c06488ff6117f44880ef8693408755617100a863
root <admin@example.com>
2020-03-29 11:17:18 -0400

    adding two missing lines

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -1,3 +1,4 @@
+
 package com.example.hellocd;
 
 import org.springframework.boot.*;
@@ -7,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class HellocdApplication {
 
 
